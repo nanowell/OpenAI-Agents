3e6c2a9ddf3757e62b88645e9bd36dfb22105b75
nanowell <zarugeos@gmail.com>
2021-09-12 01:57:17 +0300

    Add files via upload

---
+++ Stock price lookup.py
@@ -0,0 +1,21 @@
+# App which shows last week's price for Tesla Stock
+
+import streamlit as st
+import yfinance as yf
+
+st.write("""
+# Tesla Stock Price App
+Shown are the stock closing price and volume of Tesla!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'TSLA'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
