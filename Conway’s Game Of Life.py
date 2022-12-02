599acc344900b7fa5b67a4b9abb7e778beae6de1
nanowell <zarugeos@gmail.com>
2022-12-02 14:25:29 +0700

    Update Conway’s Game Of Life.py
    ChadGPT otimization
    The main changes in this optimization are the use of a list to store the coordinates of the cell's neighbors, and a for loop to iterate over that list and count the number of living neighbors. This eliminates the need for many repeated lines of code to check each neighbor individually.

--- Conway’s Game Of Life.py
+++ Conway’s Game Of Life.py
@@ -1,6 +1,3 @@
-# “Conway’s Game Of Life”
-
-
 import random
 import time
 import copy
@@ -41,26 +38,15 @@ while True: # Main program loop.
 
             # Count number of living neighbors:
             numNeighbors = 0
-            if currentCells[leftCoord][aboveCoord] == '#':
-                numNeighbors += 1 # Top-left neighbor is alive.
-            if currentCells[x][aboveCoord] == '#':
-                numNeighbors += 1 # Top neighbor is alive.
-            if currentCells[rightCoord][aboveCoord] == '#':
-                numNeighbors += 1 # Top-right neighbor is alive.
-            if currentCells[leftCoord][y] == '#':
-                numNeighbors += 1 # Left neighbor is alive.
-            if currentCells[rightCoord][y] == '#':
-                numNeighbors += 1 # Right neighbor is alive.
-            if currentCells[leftCoord][belowCoord] == '#':
-                numNeighbors += 1 # Bottom-left neighbor is alive.
-            if currentCells[x][belowCoord] == '#':
-                numNeighbors += 1 # Bottom neighbor is alive.
-            if currentCells[rightCoord][belowCoord] == '#':
-                numNeighbors += 1 # Bottom-right neighbor is alive.
+            neighborCoords = [(leftCoord, aboveCoord), (x, aboveCoord), (rightCoord, aboveCoord), 
+                              (leftCoord, y), (rightCoord, y), 
+                              (leftCoord, belowCoord), (x, belowCoord), (rightCoord, belowCoord)]
+            for coord in neighborCoords:
+                if currentCells[coord[0]][coord[1]] == '#':
+                    numNeighbors += 1
 
             # Set cell based on Conway's Game of Life rules:
-            if currentCells[x][y] == '#' and (numNeighbors == 2 or
-numNeighbors == 3):
+            if currentCells[x][y] == '#' and (numNeighbors == 2 or numNeighbors == 3):
                 # Living cells with 2 or 3 neighbors stay alive:
                 nextCells[x][y] = '#'
             elif currentCells[x][y] == ' ' and numNeighbors == 3:
