4c04368c4a08312ba7564033e4a8fdc1af200b51
nanowell <zarugeos@gmail.com>
2022-12-05 19:26:23 +0700

    Update html_parser.py
    we use the html module from the Python standard library to parse the HTML string into a DOM tree. Then, we use the xpath method to extract the <title> element from the DOM tree, and finally we extract the text inside the <title> element and print it out.

--- html_parser.py
+++ html_parser.py
@@ -1,19 +1,16 @@
-import urllib.request
-import urllib.parse
-import urllib.error
-from bs4 import BeautifulSoup
-import ssl
+import html
 
-# ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+# This is the input HTML string that we want to parse
+html_string = "<html><head><title>Hello, World!</title></head><body><h1>Hello, World!</h1></body></html>"
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+# Parse the HTML string into a DOM (Document Object Model) tree
+dom_tree = html.fromstring(html_string)
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+# Extract the title element from the DOM tree
+title_element = dom_tree.xpath('//title')[0]
+
+# Extract the text inside the title element
+title_text = title_element.text
+
+# Print the title text
+print(title_text)
