9a36801bd56bee697304627aa163ecf4450b7445
nanowell <zarugeos@gmail.com>
2022-12-05 19:31:42 +0700

    Update 4chan search.py
    This code can be further improved by using the threading module to download the images in parallel

--- 4chan search.py
+++ 4chan search.py
@@ -1,39 +1,43 @@
-
 from bs4 import BeautifulSoup
 import requests
-import urllib
+import urllib.parse
+import urllib.request
 import re
 import os
 import sys
+import threading
 
-def get_soup(url):
-    return BeautifulSoup(requests.get(url).text, "lxml")
-
-def get_main_page(board):
-    return get_soup("http://boards.4chan.org/{}/".format(board))
-
-def get_thread_page(board, thread_id):
-    return get_soup("http://boards.4chan.org/{}/res/{}".format(board, thread_id))
+def get_page_soup(board, page_type, thread_id=None):
+    base_url = "http://boards.4chan.org/{}/".format(board)
+    if page_type == "main":
+        url = base_url
+    elif page_type == "thread":
+        url = "{}res/{}".format(base_url, thread_id)
+    else:
+        raise ValueError("Invalid page type: {}".format(page_type))
+    response = requests.get(url)
+    return BeautifulSoup(response.text, "lxml")
 
 def get_thread_ids(board):
-    main_page = get_main_page(board)
+    main_page = get_page_soup(board, "main")
     thread_ids = []
     for link in main_page.find_all("a", class_="replylink"):
         thread_ids.append(link["href"][1:])
     return thread_ids
 
 def get_image_urls(thread_id):
-    thread_page = get_thread_page("g", thread_id)
+    thread_page = get_page_soup("g", "thread", thread_id)
+    base_url = thread_page.find("meta", property="og:url")["content"]
     image_urls = []
     for post in thread_page.find_all("div", class_="postContainer"):
         for link in post.find_all("a"):
             if link.has_attr("href") and link["href"].endswith("jpg"):
-                image_urls.append(link["href"])
+                image_urls.append(urllib.parse.urljoin(base_url, link["href"]))
     return image_urls
 
 def download_image(url, filename):
     print ("Downloading {} to {}".format(url, filename))
-    urllib.urlretrieve(url, filename)
+    urllib.request.urlretrieve(url, filename)
 
 def download_images(board):
     thread_ids = get_thread_ids(board)
