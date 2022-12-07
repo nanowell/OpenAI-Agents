dd1625eb22a64955f0fe4fe8c77f7c2a1b6f87c8
nanowell <zarugeos@gmail.com>
2022-12-07 23:36:04 +0700

    Update GPIO_codex.py

--- GPIO_codex.py
+++ GPIO_codex.py
@@ -1,3 +1,4 @@
+Optimize this code:
 # Check if used with Python 3
 import sys
 if sys.version_info[0] < 3:
@@ -105,45 +106,54 @@ def get_state():
 
 def main():
     """Main function"""
+    global state
+    global last_state
+    global armed_time
+    global disarmed_time
+
+    # Setup the GPIO pins
     setup_gpio()
 
-    # Register the signal handlers
-    signal.signal(signal.SIGTERM, cleanup_gpio)
-    signal.signal(signal.SIGINT, cleanup_gpio)
+    # Setup the signal handler
+    signal.signal(signal.SIGTERM, signal_handler)
 
     # Loop forever
     while True:
-        global state
-        global last_state
-        log.debug("Reading the current state")
+        # Get the current state of the system
         state = get_state()
+
+        # Check if the state has changed
         if state != last_state:
-            log.info("State changed from '%s' to '%s'", last_state, state)
+            log.info("State changed from %s to %s", last_state, state)
+
+            # Check if the system was armed
+            if state == "armed":
+                arm_system()
+            # Check if the system was disarmed
+            elif state == "disarmed":
+                disarm_system()
+
+            # Update the last state
             last_state = state
-        time.sleep(1)
+
+        # Check if the system is armed
+        if state == "armed":
+            # Check if the motion sensor is triggered
+            if read_motion() == 1:
+                log.info("Motion sensor triggered")
+                # TODO: Send an email
+
+        # Sleep for a while
+        time.sleep(0.1)
 
 
+def signal_handler(signum, frame):
+    """Signal handler"""
+    log.debug("Signal handler called with signal %d", signum)
+    cleanup_gpio()
+    sys.exit(0)
+
+
+# Run the main function
 if __name__ == "__main__":
-    # Parse the command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug", action="store_true", help="Enable debugging")
-    args = parser.parse_args()
-
-    if args.debug:
-        log.setLevel(logging.DEBUG)
-
     main()
-
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
