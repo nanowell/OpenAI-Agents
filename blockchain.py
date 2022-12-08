ce33e106d40f9c40d98a970535e6f00d6b2a750e
nanowell <zarugeos@gmail.com>
2022-12-09 00:12:49 +0700

    Update blockchain.py
    Here is a refactored version of the code with the optimization mentioned above and some additional improvements:
    
    The Block class has a next attribute added to store the next block in the chain.
    The BlockChain class has a size attribute added to keep track of the number of blocks in the chain.
    The BlockChain class has a __len__ method added to return the number of blocks in the chain.
    The BlockChain class has a __getitem__ method added to allow accessing blocks by index.
    The BlockChain class has a __iter__ and __next__ methods added to allow iterating over the blocks in the chain.
    The BlockChain class has a search method renamed to contains to better reflect its purpose.
    The BlockChain class has a get_block method renamed to get to better reflect its purpose.
    The BlockChain class has a print_list method renamed to print_chain to better reflect its purpose.
    The BlockChain class has a print_chain method added to print the blocks in the chain in a more readable format.

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,3 @@
-# sha256 hash blockchain demo
-# Data:
-# Hash:
-
-
 import hashlib
 import datetime
 
@@ -11,21 +6,47 @@ class Block:
         self.timestamp = datetime.datetime.now()
         self.data = data
         self.previous_hash = previous_hash
-        self.hash = self.calc_hash()
-
-    def calc_hash(self):
+        
         sha = hashlib.sha256()
         hash_str = "We are going to encode this string of data!".encode('utf-8')
         sha.update(hash_str)
-        return sha.hexdigest()
+        self.hash = sha.hexdigest()
+
+        self.next = None
+
+    def __str__(self):
+        return "Timestamp: {}\nData: {}\nSHA256 Hash: {}\nPrevious Hash: {}".format(self.timestamp, self.data, self.hash, self.previous_hash)
 
 class BlockChain:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, index):
+        if index >= self.size or index < 0:
+            raise IndexError("BlockChain index out of range")
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr
+
+    def __iter__(self):
+        self.curr = self.head
+        return self
+
+    def __next__(self):
+        if self.curr is None:
+            raise StopIteration()
+        curr = self.curr
+        self.curr = self.curr.next
+        return curr
 
     def append(self, data):
-        if data == None:
+        if data is None:
             return
         if not self.head:
             self.head = Block(data, 0)
@@ -33,9 +54,10 @@ class BlockChain:
         else:
             self.tail.next = Block(data, self.tail.hash)
             self.tail = self.tail.next
+        self.size += 1
 
-    def search(self, data):
-        if data == None:
+    def contains(self, data):
+        if data is None:
             return False
         curr = self.head
         while curr:
@@ -44,8 +66,8 @@ class BlockChain:
             curr = curr.next
         return False
 
-    def get_block(self, data):
-        if data == None:
+    def get(self, data):
+        if data is None:
             return None
         curr = self.head
         while curr:
@@ -54,20 +76,24 @@ class BlockChain:
             curr = curr.next
         return None
 
-    def print_list(self):
+    def print_chain(self):
         curr = self.head
         while curr:
-            print("Timestamp: {}\nData: {}\nSHA256 Hash: {}\nPrevious Hash: {}\n".format(curr.timestamp, curr.data, curr.hash, curr.previous_hash))
+            print(curr)
             curr = curr.next
 
 def main():
     block_chain = BlockChain()
     block_chain.append("Some Information")
     block_chain.append("Another piece of information")
-    block_chain.print_list()
-    print(block_chain.search("Some Information"))
-    print(block_chain.search("None"))
-    print(block_chain.get_block("Another piece of information").timestamp)
+    block_chain.print_chain()
+    print("Size:", len(block_chain))
+    print("Contains 'Some Information':", block_chain.contains("Some Information"))
+    print("Contains 'None':", block_chain.contains("None"))
+    print("Timestamp of second block:", block_chain[1].timestamp)
+    print("Iterate over blocks:")
+    for block in block_chain:
+        print(block)
 
 if __name__ == '__main__':
     main()
