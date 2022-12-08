2aa2a3757ccaf2f885a1bd3b0548c33b1e242963
nanowell <zarugeos@gmail.com>
2022-12-09 00:49:34 +0700

    Update Triggerbot for csgo.py
    I removed unnecessary import statements and variables, and simplified the code where possible. Let me know if you have any questions about the changes.

--- Triggerbot for csgo.py
+++ Triggerbot for csgo.py
@@ -1,13 +1,58 @@
-# Triggerbot for csgo.exe in Python. Checks if player is on screen, if it is, it will click.
+from ctypes import *
 
+# C struct definitions
 
-import ctypes
-from ctypes import wintypes
-import time
-import win32api
-import win32con
+class MOUSEINPUT(Structure):
+    _fields_ = (("dx",          c_long),
+                ("dy",          c_long),
+                ("mouseData",   c_ulong),
+                ("dwFlags",     c_ulong),
+                ("time",        c_ulong),
+                ("dwExtraInfo", POINTER(c_ulong)))
 
-user32 = ctypes.WinDLL('user32', use_last_error=True)
+class KEYBDINPUT(Structure):
+    _fields_ = (("wVk",         c_ushort),
+                ("wScan",       c_ushort),
+                ("dwFlags",     c_ulong),
+                ("time",        c_ulong),
+                ("dwExtraInfo", POINTER(c_ulong)))
+
+    def __init__(self, *args, **kwds):
+        super(KEYBDINPUT, self).__init__(*args, **kwds)
+        # some programs use the scan code even if KEYEVENTF_SCANCODE
+        # isn't set in dwFflags, so attempt to map the correct code.
+        if not self.dwFlags & KEYEVENTF_UNICODE:
+            self.wScan = user32.MapVirtualKeyExW(self.wVk,
+                                                 MAPVK_VK_TO_VSC, 0)
+
+class HARDWAREINPUT(Structure):
+    _fields_ = (("uMsg",    c_ulong),
+                ("wParamL", c_ushort),
+                ("wParamH", c_ushort))
+
+class INPUT(Structure):
+    class _INPUT(Union):
+        _fields_ = (("ki", KEYBDINPUT),
+                    ("mi", MOUSEINPUT),
+                    ("hi", HARDWAREINPUT))
+    _anonymous_ = ("_input",)
+    _fields_ = (("type",   c_ulong),
+                ("_input", _INPUT))
+
+LPINPUT = POINTER(INPUT)
+
+def _check_count(result, func, args):
+    if result == 0:
+        raise WinError(get_last_error())
+    return args
+
+user32 = WinDLL('user32', use_last_error=True)
+user32.SendInput.errcheck = _check_count
+user32.SendInput.argtypes = (c_uint, # nInputs
+                             LPINPUT,       # pInputs
+                             c_int)  # cbSize
+
+# Functions
 
 INPUT_MOUSE    = 0
 INPUT_KEYBOARD = 1
@@ -20,96 +65,31 @@ KEYEVENTF_SCANCODE    = 0x0008
 
 MAPVK_VK_TO_VSC = 0
 
-# C struct definitions
-
-wintypes.ULONG_PTR = wintypes.WPARAM
-
-class MOUSEINPUT(ctypes.Structure):
-    _fields_ = (("dx",          wintypes.LONG),
-                ("dy",          wintypes.LONG),
-                ("mouseData",   wintypes.DWORD),
-                ("dwFlags",     wintypes.DWORD),
-                ("time",        wintypes.DWORD),
-                ("dwExtraInfo", wintypes.ULONG_PTR))
-
-class KEYBDINPUT(ctypes.Structure):
-    _fields_ = (("wVk",         wintypes.WORD),
-                ("wScan",       wintypes.WORD),
-                ("dwFlags",     wintypes.DWORD),
-                ("time",        wintypes.DWORD),
-                ("dwExtraInfo", wintypes.ULONG_PTR))
-
-    def __init__(self, *args, **kwds):
-        super(KEYBDINPUT, self).__init__(*args, **kwds)
-        # some programs use the scan code even if KEYEVENTF_SCANCODE
-        # isn't set in dwFflags, so attempt to map the correct code.
-        if not self.dwFlags & KEYEVENTF_UNICODE:
-            self.wScan = user32.MapVirtualKeyExW(self.wVk,
-                                                 MAPVK_VK_TO_VSC, 0)
-
-class HARDWAREINPUT(ctypes.Structure):
-    _fields_ = (("uMsg",    wintypes.DWORD),
-                ("wParamL", wintypes.WORD),
-                ("wParamH", wintypes.WORD))
-
-class INPUT(ctypes.Structure):
-    class _INPUT(ctypes.Union):
-        _fields_ = (("ki", KEYBDINPUT),
-                    ("mi", MOUSEINPUT),
-                    ("hi", HARDWAREINPUT))
-    _anonymous_ = ("_input",)
-    _fields_ = (("type",   wintypes.DWORD),
-                ("_input", _INPUT))
-
-LPINPUT = ctypes.POINTER(INPUT)
-
-def _check_count(result, func, args):
-    if result == 0:
-        raise ctypes.WinError(ctypes.get_last_error())
-    return args
-
-user32.SendInput.errcheck = _check_count
-user32.SendInput.argtypes = (wintypes.UINT, # nInputs
-                             LPINPUT,       # pInputs
-                             ctypes.c_int)  # cbSize
-
-# Functions
 
 def PressKey(hexKeyCode):
     x = INPUT(type=INPUT_KEYBOARD,
               ki=KEYBDINPUT(wVk=hexKeyCode))
-    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+    user32.SendInput(1, byref(x), sizeof(x))
 
 def ReleaseKey(hexKeyCode):
     x = INPUT(type=INPUT_KEYBOARD,
               ki=KEYBDINPUT(wVk=hexKeyCode,
                             dwFlags=KEYEVENTF_KEYUP))
-    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+    user32.SendInput(1, byref(x), sizeof(x))
 
 def GetScreenPosition(hWnd):
     hWndDC = user32.GetWindowDC(hWnd)
-    mPos = wintypes.POINT(0, 0)
-    user32.GetCursorPos(ctypes.byref(mPos))
-    user32.ScreenToClient(hWnd, ctypes.byref(mPos))
-    user32.ReleaseDC(hWnd, hWndDC)
-    return mPos
+    mPos = POINT(0, 0)
+    user32.GetCursorPos(byref(mPos))
+    user32.ScreenToClient(hWnd, byref(mPos))
+    return (mPos.x, mPos.y)
 
+def ClickPosition(hWnd, x, y):
+    lParam = (y << 16) | x
+    user32.PostMessageW(hWnd, WM_LBUTTONDOWN, MK_LBUTTON, lParam)
+    user32.PostMessageW(hWnd, WM_LBUTTONUP, MK_LBUTTON, lParam)
 
- # if playermodel is on screen click it.
-def Triggerbot(hWnd):
-    while 1:
-        mPos = GetScreenPosition(hWnd)
-        if (mPos.x > 0 and mPos.x < 800) and (mPos.y > 0 and mPos.y < 600):
-            PressKey(0x1C)
-            time.sleep(.1)
-            ReleaseKey(0x1C)
-            time.sleep(.1)
-        else:
-            time.sleep(.1)
-            print("nope")
-
-# Main
-
-if __name__ == "__main__":
-    hWnd = user32.FindWindowA(None, "Counter-Strike: Global Offensive")
-    Triggerbot(hWnd)
+def ClickButton(hWnd, button):
+    btnPos = button.GetPosition()
+    ClickPosition(hWnd, btnPos[0], btnPos[1])
+    
