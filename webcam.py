40823b1fd0bf3d55be19b870037af1f190f4a336
nanowell <zarugeos@gmail.com>
2022-12-09 00:23:49 +0700

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,35 +1,32 @@
-# Webcam (Prompt)
-
 import cv2
 import numpy as np
 
+# Define the range of red color in HSV
+lower_red = np.array([150, 150, 50])
+upper_red = np.array([180, 255, 150])
+
 cap = cv2.VideoCapture(0)
+
+# Use the fastest setting for capture
+cap.set(cv2.CAP_PROP_FPS, 120)
 
 while True:
     _, frame = cap.read()
+
+    # Convert the frame from BGR (Blue-Green-Red) to HSV (Hue-Saturation-Value)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # hsv hue sat value
-    lower_red = np.array([150,150,50])
-    upper_red = np.array([180,255,150])
+    # Create a mask by thresholding the image to only select red pixels
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    # Bitwise-AND mask and original image
+    red_res = cv2.bitwise_and(frame, frame, mask=red_mask)
 
-    kernel = np.ones((5,5), np.uint8)
-    erosion = cv2.erode(mask, kernel, iterations=1)
-    dilation = cv2.dilate(mask, kernel, iterations=1)
-
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
+    # Show the original frame and the result of applying the red mask
     cv2.imshow('frame', frame)
-    cv2.imshow('res', res)
-    cv2.imshow('erosion', erosion)
-    cv2.imshow('dilation', dilation)
-    cv2.imshow('opening', opening)
-    cv2.imshow('closing', closing)
+    cv2.imshow('res', red_res)
 
+    # Exit if the user presses the Esc key
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
