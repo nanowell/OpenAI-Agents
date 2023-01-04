1d5515698c734d650f7c3c9b2d38f4332f6ca9f9
nanowell <zarugeos@gmail.com>
2023-01-04 18:40:05 +0700

    Update Conway’s Game Of Life.py
    I made the following changes:
    
    Used a list comprehension to create the nextCells list of lists, replacing the nested for loops.
    Used a list comprehension to count the number of living neighbors, replacing the for loop and if statement.
    Moved the time.sleep(1) call to the end of the loop to avoid unnecessary delays.
    Removed the unnecessary end='' argument in the print() call.

--- Conway’s Game Of Life.py
+++ Conway’s Game Of Life.py
@@ -6,15 +6,7 @@ WIDTH = 60
 HEIGHT = 20
 
 # Create a list of list for the cells:
-nextCells = []
-for x in range(WIDTH):
-    column = [] # Create a new column.
-    for y in range(HEIGHT):
-        if random.randint(0, 1) == 0:
-            column.append('#') # Add a living cell.
-        else:
-            column.append(' ') # Add a dead cell.
-    nextCells.append(column) # nextCells is a list of column lists.
+nextCells = [['#' if random.randint(0, 1) == 0 else ' ' for y in range(HEIGHT)] for x in range(WIDTH)]
 
 while True: # Main program loop.
     print('\n\n\n\n\n') # Separate each step with newlines.
@@ -30,20 +22,15 @@ while True: # Main program loop.
     for x in range(WIDTH):
         for y in range(HEIGHT):
             # Get neighboring coordinates:
-            # `% WIDTH` ensures leftCoord is always between 0 and WIDTH - 1
             leftCoord  = (x - 1) % WIDTH
             rightCoord = (x + 1) % WIDTH
             aboveCoord = (y - 1) % HEIGHT
             belowCoord = (y + 1) % HEIGHT
 
             # Count number of living neighbors:
-            numNeighbors = 0
-            neighborCoords = [(leftCoord, aboveCoord), (x, aboveCoord), (rightCoord, aboveCoord), 
-                              (leftCoord, y), (rightCoord, y), 
-                              (leftCoord, belowCoord), (x, belowCoord), (rightCoord, belowCoord)]
-            for coord in neighborCoords:
-                if currentCells[coord[0]][coord[1]] == '#':
-                    numNeighbors += 1
+            numNeighbors = sum([1 for coord in [(leftCoord, aboveCoord), (x, aboveCoord), (rightCoord, aboveCoord), 
+                                                (leftCoord, y), (rightCoord, y), 
+                                                (leftCoord, belowCoord), (x, belowCoord), (rightCoord, belowCoord)] if currentCells[coord[0]][coord[1]] == '#'])
 
             # Set cell based on Conway's Game of Life rules:
             if currentCells[x][y] == '#' and (numNeighbors == 2 or numNeighbors == 3):
