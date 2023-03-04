225467dfcc590a7d37174717c07120e4df8b4f7e
nanowell <zarugeos@gmail.com>
2023-03-05 00:44:27 +0700

    Update Conway’s Game Of Life.py
    GPT-4 optimizations

--- Conway’s Game Of Life.py
+++ Conway’s Game Of Life.py
@@ -1,45 +1,34 @@
-import random
+import numpy as np
 import time
-import copy
 
 WIDTH = 60
 HEIGHT = 20
 
-# Create a list of list for the cells:
-nextCells = [['#' if random.randint(0, 1) == 0 else ' ' for y in range(HEIGHT)] for x in range(WIDTH)]
+# Create a random array of 0s and 1s for the cells:
+nextCells = np.random.randint(0, 2, size=(WIDTH, HEIGHT))
 
 while True: # Main program loop.
     print('\n\n\n\n\n') # Separate each step with newlines.
-    currentCells = copy.deepcopy(nextCells)
+    currentCells = nextCells.copy()
 
     # Print currentCells on the screen:
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            print(currentCells[x][y], end='') # Print the # or space.
+            print('#' if currentCells[x][y] == 1 else ' ', end='') # Print the # or space.
         print() # Print a newline at the end of the row.
 
     # Calculate the next step's cells based on current step's cells:
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            # Get neighboring coordinates:
-            leftCoord  = (x - 1) % WIDTH
-            rightCoord = (x + 1) % WIDTH
-            aboveCoord = (y - 1) % HEIGHT
-            belowCoord = (y + 1) % HEIGHT
-
-            # Count number of living neighbors:
-            numNeighbors = sum([1 for coord in [(leftCoord, aboveCoord), (x, aboveCoord), (rightCoord, aboveCoord), 
-                                                (leftCoord, y), (rightCoord, y), 
-                                                (leftCoord, belowCoord), (x, belowCoord), (rightCoord, belowCoord)] if currentCells[coord[0]][coord[1]] == '#'])
-
-            # Set cell based on Conway's Game of Life rules:
-            if currentCells[x][y] == '#' and (numNeighbors == 2 or numNeighbors == 3):
-                # Living cells with 2 or 3 neighbors stay alive:
-                nextCells[x][y] = '#'
-            elif currentCells[x][y] == ' ' and numNeighbors == 3:
-                # Dead cells with 3 neighbors become alive:
-                nextCells[x][y] = '#'
-            else:
-                # Everything else dies or stays dead:
-                nextCells[x][y] = ' '
-    time.sleep(1) # Add a 1-second pause to reduce flickering.
+    
+    # Create an array that stores the number of living neighbors for each cell:
+    neighbors = np.zeros((WIDTH, HEIGHT), dtype=int)
+    
+    # Shift the array in eight directions and add them up:
+    for dx, dy in [(-1,-1), (-1,0), (-1,+1), (0,-1), (0,+1), (+1,-1), (+1,0), (+1,+1)]:
+        neighbors += np.roll(np.roll(currentCells, dx, axis=0), dy, axis=1)
+    
+    # Apply the rules of the game using np.where:
+    nextCells = np.where(currentCells == 1,
+                         np.where((neighbors == 2) | (neighbors == 3), 1 , 0),
+                         np.where(neighbors == 3 , 1 , 0))
+    
+    time.sleep(0.1) # Add a 1-second pause to reduce flickering.
