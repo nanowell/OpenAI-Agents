c4b5ac4a2179648a9dd9e14254b6b0762d3b0e0b
nanowell <zarugeos@gmail.com>
2023-03-27 13:44:31 +0700

    Update raspberry_pi_kernel_builder.py
    GUI

--- python/raspberry_pi_kernel_builder.py
+++ python/raspberry_pi_kernel_builder.py
@@ -1,148 +1,139 @@
-from __future__ import print_function
 import os
-import re
 import shutil
 import subprocess
 import sys
-import tempfile
 
-# Package dependencies:
-#
-# build-essential
-# libncurses5-dev
-# libssl-dev
-
-# -----------------------------------------------------------------------------
-
-def main():
-    """
-    Main program.
-
-    """
-
-    build_dir = tempfile.mkdtemp()
-    os.chdir(build_dir)
-
-    print("Downloading kernel source...")
-    if not download_source():
-        print("Error downloading kernel source.  Exiting...")
-        sys.exit(1)
-
-    print("Building kernel...")
-    if not build_kernel():
-        print("Error building kernel.  Exiting...")
-        sys.exit(1)
-
-    print("Installing kernel...")
-    if not install_kernel():
-        print("Error installing kernel.  Exiting...")
-        sys.exit(1)
-
-    print("Cleaning up...")
-    if not cleanup():
-        print("Error cleaning up.  Exiting...")
-        sys.exit(1)
-
-    print("Finished.")
-    sys.exit(0)
-
-# -----------------------------------------------------------------------------
-
-def build_kernel():
-    """
-    Build the kernel.
-
-    """
-
-    # Configure the build.
-    if not run_command("./build_rpi_kernel.sh"):
-        return False
-
-    # Build the kernel.
-    if not run_command("make -j4"):
-        return False
-
-    return True
-
-# -----------------------------------------------------------------------------
-
-def build_modules():
-    """
-    Build kernel modules.
-
-    """
-
-    # Build the kernel modules.
-    if not run_command("make -C /lib/modules/$(uname -r)/build M=$(pwd) modules"):
-        return False
-
-    return True
-
-# -----------------------------------------------------------------------------
-
-def cleanup():
-    """
-    Clean up the build directory.
-
-    """
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QStatusBar,
+)
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-    return True
+        self.build_button = QPushButton("Build Kernel")
+        self.build_dir_label = QLabel("Build Directory:")
+        self.build_dir_edit = QLineEdit()
+        self.download_button = QPushButton("Download Source")
+        self.status_label = QLabel("")
 
-# -----------------------------------------------------------------------------
+        self.build_button.clicked.connect(self.build_kernel)
+        self.download_button.clicked.connect(self.download_source)
 
-def download_source():
-    """
-    Download the kernel source.
+        build_dir_layout = QHBoxLayout()
+        build_dir_layout.addWidget(self.build_dir_label)
+        build_dir_layout.addWidget(self.build_dir_edit)
 
-    """
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.build_button)
+        button_layout.addWidget(self.download_button)
 
-    # Download the source.
-    if not run_command("wget https://github.com/raspberrypi/linux/archive/rpi-3.18.y.zip"):
-        return False
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(build_dir_layout)
+        main_layout.addLayout(button_layout)
+        main_layout.addWidget(self.status_label)
 
-    # Unzip the source.
-    if not run_command("unzip rpi-3.18.y.zip"):
-        return False
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
 
-    return True
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
 
-# -----------------------------------------------------------------------------
+        self.setWindowTitle("Raspberry Pi Kernel Builder")
+        self.resize(400, 300)
 
-def install_kernel():
-    """
-    Install the kernel.
+    def build_kernel(self):
+        build_dir = self.build_dir_edit.text()
+        if not build_dir:
+            self.status_label.setText("Please enter build directory")
+            return
 
-    """
+        self.status_label.setText("Building kernel...")
 
-    # Install the kernel.
-    if not run_command("sudo make modules_install"):
-        return False
+        os.chdir(build_dir)
 
-    # Install the kernel headers.
-    if not run_command("sudo make headers_install"):
-        return False
+        if not self.run_command("./build_rpi_kernel.sh"):
+            self.status_label.setText("Error building kernel")
+            return
 
-    return True
+        if not self.run_command("make -j4"):
+            self.status_label.setText("Error building kernel")
+            return
 
-# -----------------------------------------------------------------------------
+        if not self.install_kernel():
+            self.status_label.setText("Error installing kernel")
+            return
 
-def run_command(command):
-    """
-    Run a command.
+        if not self.cleanup():
+            self.status_label.setText("Error cleaning up")
+            return
 
-    """
+        self.status_label.setText("Finished.")
 
-    try:
-        subprocess.check_call(command, shell=True)
-    except subprocess.CalledProcessError as err:
-        print("Command failed: " + str(err))
-        return False
+    def download_source(self):
+        build_dir = self.build_dir_edit.text()
+        if not build_dir:
+            self.status_label.setText("Please enter build directory")
+            return
 
-    return True
+        self.status_label.setText("Downloading kernel source...")
 
-# -----------------------------------------------------------------------------
+        os.chdir(build_dir)
+
+        if not self.run_command("wget https://github.com/raspberrypi/linux/archive/rpi-3.18.y.zip"):
+            self.status_label.setText("Error downloading kernel source")
+            return
+
+        if not self.run_command("unzip rpi-3.18.y.zip"):
+            self.status_label.setText("Error unzipping kernel source")
+            return
+
+        self.status_label.setText("Kernel source downloaded successfully")
+
+    def install_kernel(self):
+        self.status_label.setText("Installing kernel...")
+
+        if not self.run_command("sudo make modules_install"):
+            return False
+
+        if not self.run_command("sudo make headers_install"):
+            return False
+
+        return True
+
+    def cleanup(self):
+        self.status_label.setText("Cleaning up...")
+
+        build_dir = self.build_dir_edit.text()
+
+        if os.path.exists(build_dir):
+            shutil.rmtree(build_dir)
+
+        return True
+
+    def run_command(self, command):
+        try:
+            subprocess.check_call(command, shell=True)
+        except subprocess.CalledProcessError as err:
+            self.status_label.setText("Command failed: " + str(err))
+            return False
+
+        return True
+
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
